6589c1cbba06147af14d9a57d08758d74379b69c
Abhishek N S <abhishek.ns@shopalyst.com>
2024-01-27 12:53:54 +0530

    file upload multer, cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs'; // file system is used to read, write the file. 
+// The Node.js file system module allows you to work with the file system on your computer.
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localfilepath) => {
+    try {
+        if (!localfilepath) return null;
+        // Upload the file to the cloudinary
+        const response = await cloudinary.uploader.upload(localfilepath, {
+            resource_type: "auto"
+        })
+        // File is successfully uploaded.
+        console.log("File is successfully uploaded.", response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localfilepath)
+        // remove the locally saved temporary file as the upload operation got failed.
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
