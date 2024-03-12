d8eb83ad2baf296652628d3ffead300346e7a5fe
Abhishek N S <abhishek.ns@shopalyst.com>
2024-03-12 22:58:22 +0530

    get subscribed channels

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -12,6 +12,14 @@ const toggleSubscription = asyncHandler(async (req, res) => {
         throw new ApiError(404, "Channel Id is required")
     }
 
+    const checkSubscriber = await Subscription.find({
+        subscriber: req.user?._id
+    })
+
+    if (checkSubscriber){
+        throw new ApiError(404, "User Subscribed already")
+    }
+    
     const subscribe = await Subscription.create(
         {
             subscriber: req.user?._id,
@@ -89,4 +97,61 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
         subscribers: subscribedList}, "Subscribed list fetched successfully"))
 })
 
-export {toggleSubscription, getUserChannelSubscribers}
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler( async(req, res) => {
+        const { channelId } = req.params;
+
+    if (!channelId) {
+        throw new ApiError(404, "subscriber Id is required")
+    }
+
+    if (!mongoose.isValidObjectId(channelId)) {
+        throw new ApiError(404, "Invalid Subscribe Id has been provided")
+    }
+
+    const subscribersList = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscriber",
+                foreignField: "_id",
+                as: "subscribers",
+                pipeline: [
+                    {
+                        $project: {
+                            fullName: 1,
+                            userName: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$subscribers"
+        },
+        {
+            $replaceRoot: {
+                newRoot: "$subscribers"
+            }
+        }
+    ])
+
+    if (!subscribersList?.length) {
+        throw new ApiError(404, "Subscribers not found")
+    }
+
+    res
+    .status(200)
+    .json(new ApiResponse(200, {
+        channelId: channelId,
+        subscribersCount: subscribersList?.length,
+        subscribers: subscribersList}, "Subscribers list fetched successfully"))
+})
+
+export {toggleSubscription, getUserChannelSubscribers, getSubscribedChannels}
--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { toggleSubscription, getUserChannelSubscribers } from "../controllers/subscription.controller.js";
+import { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels } from "../controllers/subscription.controller.js";
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router = Router();
 router.use(verifyJWT)
 
-router.route("/c/:channelId").post(toggleSubscription)
+router.route("/c/:channelId").post(toggleSubscription).get(getSubscribedChannels)
 
 router.route("/u/:subscriberId").get(getUserChannelSubscribers);
 
